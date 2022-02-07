4dec40b739dc715048ed3d615756b18f03851152
zlw-qa <asdori95@gmail.com>
2022-02-07 23:52:50 +0400

    add App

--- src/test/java/com/github/zlwqa/BookStoreTests.java
+++ src/test/java/com/github/zlwqa/BookStoreTests.java
@@ -5,16 +5,13 @@ import annotations.JiraIssues;
 import annotations.Layer;
 import annotations.Microservice;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.github.zlwqa.config.ApiConfig;
-import com.github.zlwqa.config.CredentialsConfig;
-import com.github.zlwqa.lombok.UserRequestData;
 import com.github.zlwqa.lombok.UserResponseData;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.*;
 
+import static com.github.zlwqa.config.App.*;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -28,29 +25,12 @@ import static org.hamcrest.Matchers.notNullValue;
 @DisplayName("Тестирование веб-приложения Book Store")
 public class BookStoreTests {
 
-    private static final CredentialsConfig CREDENTIALS_CONFIG =
-            ConfigFactory.create(CredentialsConfig.class, System.getProperties());
-    private static final String USER_NAME = CREDENTIALS_CONFIG.userName();
-    private static final String PASSWORD = CREDENTIALS_CONFIG.password();
-
-    public static final ApiConfig API_CONFIG = ConfigFactory.create(ApiConfig.class, System.getProperties());
-
-    private static final UserResponseData USER_RESPONSE_DATA = new UserResponseData();
-    private static final UserRequestData USER_REQUEST_DATA = new UserRequestData();
-
-    public static UserRequestData setUserLoginData() {
-        USER_REQUEST_DATA.setUserName(USER_NAME);
-        USER_REQUEST_DATA.setPassword(PASSWORD);
-        return USER_REQUEST_DATA;
-    }
-
-
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         RestAssured.baseURI = API_CONFIG.apiUrl();
 
-        step("Получение токена авторизация и userId", () -> {
+        step("Получение токена авторизации и userId", () -> {
             UserResponseData userResponseData = given()
                     .contentType(JSON)
                     .body(setUserLoginData())
@@ -84,18 +64,56 @@ public class BookStoreTests {
                 .statusCode(200)
                 .body("token", notNullValue(),
                         "status", is("Success"),
-                        "result", is("UserRequestData authorized successfully."));
+                        "result", is("User authorized successfully."));
     }
 
     @Test
-    @DisplayName("Добавление книги")
-    @Tags({@Tag("Critical"), @Tag("Highest")})
-    @Microservice("Generation Token")
-    @Feature("Генерация токена")
-    @Story("Метод /BookStore/v1/Books")
-    @Severity(SeverityLevel.CRITICAL)
-    @Link(name = "Book Store", url = "https://demoqa.com/books")
-    void addBookTest() {
+    @DisplayName("Отображение списка всех книг")
+    @Tags({@Tag("Major"), @Tag("Medium")})
+    @Microservice("BookStore")
+    @Feature("Список книг")
+    @Story("Метод GET /BookStore/v1/Books")
+    @Severity(SeverityLevel.NORMAL)
+    void displayAListOfAllBooksTest() {
+        given()
+                .contentType(JSON)
+                .when()
+                .get("/BookStore/v1/Books")
+                .then().log().all()
+                .statusCode(200)
+                .body("books", notNullValue(),
+                        "books[0].isbn", is("9781449325862"),
+                        "books[0].title", is("Git Pocket Guide"));
+    }
+
+    @Test
+    @DisplayName("Отображение определенной книги по ISBN в списке всех книг")
+    @Tags({@Tag("Major"), @Tag("Medium")})
+    @Microservice("BookStore")
+    @Feature("Список книг")
+    @Story("Метод GET /BookStore/v1/Book?ISBN=")
+    @Severity(SeverityLevel.NORMAL)
+    void displayABookByISBNInTheListOfAllBooksTest() {
+        given()
+                .contentType(JSON)
+                .formParam("ISBN", "9781449325862")
+                .when()
+                .get("/BookStore/v1/Book")
+                .then().log().all()
+                .statusCode(200)
+                .body(notNullValue())
+                .body("isbn", is("9781449325862"),
+                        "title", is("Git Pocket Guide"));
+    }
+
+    @Test
+    @DisplayName("Добавление книги в профиль пользователя")
+    @Tags({@Tag("Blocker"), @Tag("High")})
+    @Microservice("BookStore")
+    @Feature("Список добавленных книг в профиле пользователя")
+    @Story("Метод POST /BookStore/v1/Books")
+    @Severity(SeverityLevel.BLOCKER)
+    static void addingABookToAUserProfileTest() {
         String data = "{\"userId\": \"" + USER_RESPONSE_DATA.getUserId() + "\"," +
                 "\"collectionOfIsbns\" : [{\"isbn\":\"9781449325862\"}]}";
 
@@ -106,7 +124,32 @@ public class BookStoreTests {
                 .when()
                 .post("/BookStore/v1/Books")
                 .then().log().all()
-                .statusCode(201);
+                .statusCode(201)
+                .body("books[0].isbn", is("9781449325862"));
+    }
+
+    @Test
+    @DisplayName("Удаление добавленной книги из профиля пользователя")
+    @Tags({@Tag("Blocker"), @Tag("High")})
+    @Microservice("BookStore")
+    @Feature("Список добавленных книг в профиле пользователя")
+    @Story("Метод DELETE /BookStore/v1/Book")
+    @Severity(SeverityLevel.BLOCKER)
+    static void removingAnAddedBookFromAUserProfileTest() {
+
+        String data = "{\"isbn\":\"9781449325862\"," +
+                "userId\": \"" + USER_RESPONSE_DATA.getUserId() + "\"}";
+
+        given()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + USER_RESPONSE_DATA.getToken())
+                .baseUri("https://demoqa.com")
+                .body(data)
+                .when()
+                .delete("/BookStore/v1/Book")
+                .then().log().all()
+                .statusCode(204)
+                .body(is(""));
     }
 }
 
